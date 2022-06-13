005936cbaa26b84da73fe7a23d8c2a6f7d10f2b3
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-06-13 10:53:55 +0200

    register_login

--- login_register/views.py
+++ login_register/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.forms import UserCreationForm
+from .forms import User_creation
 
 # Create your views here.
 
 def login_user(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
@@ -24,7 +24,16 @@ def logout_user(request):
     return redirect('login_page')
 
 def register_user(request):
+    form = User_creation()
+    if request.method == 'POST':
+        form = User_creation(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.save()
+            if user is not None:
+                login(request,user)
+                return redirect('home_page')
 
-    return render(request,'register.html',{})
+    return render(request,'register.html',{'forma_register':form})
 
 
